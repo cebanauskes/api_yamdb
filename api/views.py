171d567708bd8a1b25a93f763cd7a88a85c49fe8
schetininl <salexbin@bk.ru>
2020-04-28 22:36:55 +0300

    lookaup_feild = username

--- api/views.py
+++ api/views.py
@@ -9,11 +9,5 @@ from .serializers import UserSerializer
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    lookup_field = 'username'
     permission_classes = [permissions.IsAuthenticated]
-
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
