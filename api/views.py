0212fb783da0ee53dc08785a5071b026cc86c047
schetininl <salexbin@bk.ru>
2020-04-28 23:00:59 +0300

    Custom permissions added

--- api/views.py
+++ api/views.py
@@ -4,10 +4,11 @@ from rest_framework.response import Response
 
 from .models import User
 from .serializers import UserSerializer
+from .permissions import IsAdmin
 
 
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
     lookup_field = 'username'
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAdmin]
