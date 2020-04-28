a442f57f6f04297a75df03a52556b80c7481dc19
schetininl <salexbin@bk.ru>
2020-04-28 20:55:18 +0300

    update User model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('moderator', 'Moderator'),
+        ('user', 'User'),
+    )
+
+    role = models.CharField(
+        max_length=9,
+        choices=ROLE_CHOICES,
+        default='user',
+    )
+
+    description = models.TextField(max_length=500, blank=True)
