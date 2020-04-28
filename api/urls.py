6ab33069dc5f5006289363e8893b2f4ff6b20816
schetininl <salexbin@bk.ru>
2020-04-28 20:40:46 +0300

    Signature JWT added

---
+++ api/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+        TokenObtainPairView,
+        TokenRefreshView,
+    )
+
+
+router = DefaultRouter()
+
+
+urlpatterns = [
+    path('v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
