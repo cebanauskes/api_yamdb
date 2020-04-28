7c87649c0e6fcbda47b3e3d0227bc84a30e7b65b
schetininl <salexbin@bk.ru>
2020-04-29 00:38:15 +0300

    refactoring

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from rest_framework.validators import UniqueValidator, UniqueTogetherValidator
+from rest_framework.validators import UniqueValidator
 from rest_framework.serializers import EmailField, CharField
 
 from .models import User
--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,5 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import (
-        TokenObtainPairView,
-        TokenRefreshView,
-    )
 
 from .views import send_confirmation_code, get_jwt_token, UserViewSet, APIUser
 
