e4d1330d373feb5cef5fc99915db73ca0f11d863
schetininl <salexbin@bk.ru>
2020-04-29 15:56:03 +0300

    models corrections

--- api/serializers.py
+++ api/serializers.py
@@ -6,17 +6,6 @@ from .models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
-    email = EmailField(
-        max_length=50,
-        validators=[UniqueValidator(queryset=User.objects.all())],
-        allow_blank=False
-    )
-
-    username = CharField(
-        max_length=40,
-        validators=[UniqueValidator(queryset=User.objects.all())],
-        allow_blank=False
-    )
 
     class Meta:
         fields = ('first_name', 'last_name', 'username', 'bio', 'email', 'role', )
