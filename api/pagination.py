c74eabc2a3b8d7cfa7e71bf31702e2594a7f0031
Georgii Naumov <gmnaumov@yandex.ru>
2020-05-06 21:36:38 +0300

    API_YAMDB_GEORGII_0.0.2

--- api/pagination.py
+++ api/pagination.py
@@ -5,20 +5,26 @@ from rest_framework.response import Response
 class GenrePagination(pagination.PageNumberPagination):
     def get_paginated_response(self, data):
         return Response({
-            'links': {
-
-            },
+            # 'links': {
+            #     'next': self.get_next_link(),
+            #     'previous': self.get_previous_link()
+            # },
             'count': self.page.paginator.count,
-            'response': data,
+            'results': data,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link()
         })
 
 
 class CategoryPagination(pagination.PageNumberPagination):
     def get_paginated_response(self, data):
         return Response({
-            'links': {
-
-            },
+            # 'links': {
+            #     'next': self.get_next_link(),
+            #     'previous': self.get_previous_link()
+            # },
             'count': self.page.paginator.count,
-            'response': data,
+            'results': data,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link()
         })
--- api/views.py
+++ api/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, filters, status
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from rest_framework_simplejwt.tokens import AccessToken
+from rest_framework.pagination import PageNumberPagination
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
 
@@ -78,7 +80,8 @@ class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     pagination_class = CategoryPagination
-    permission_classes = [IsAdmin]
+    #pagination_class = PageNumberPagination
+    permission_classes = [IsAuthenticatedOrReadOnly]
     lookup_field = 'slug'
 
 
@@ -86,7 +89,8 @@ class GenreViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
     pagination_class = GenrePagination
-    permission_classes = [IsAdmin]
+    #pagination_class = PageNumberPagination
+    permission_classes = [IsAuthenticatedOrReadOnly]
     lookup_field = 'slug'
 
 
