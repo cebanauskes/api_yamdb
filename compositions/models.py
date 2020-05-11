77d6aba8b60bd166865069ff79da461442e333f5
Georgii Naumov <gmnaumov@yandex.ru>
2020-05-11 15:16:28 +0300

    API_YAMDB_DEVGEORGII_0.1.2

--- compositions/models.py
+++ compositions/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 from users.models import User
 
@@ -22,21 +23,10 @@ class Title(models.Model):
 
 
 class Review(models.Model):
-    RATING_CHOICES = [
-        (1, '1'),
-        (2, '2'),
-        (3, '3'),
-        (4, '4'),
-        (5, '5'),
-        (6, '6'),
-        (7, '7'),
-        (8, '8'),
-        (9, '9'),
-        (10, '10'), ]
     text = models.TextField(max_length=3000)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews')
-    score = models.IntegerField(choices=RATING_CHOICES)
-    pub_date = models.DateTimeField("Дата публикации", auto_now_add=True)
+    score = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)])
+    pub_date = models.DateTimeField(auto_now_add=True)
     title = models.ForeignKey(Title, on_delete=models.CASCADE, related_name='reviews')
 
 
@@ -44,4 +34,4 @@ class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
     review = models.ForeignKey(Review, on_delete=models.CASCADE, related_name="comments")
     text = models.TextField()
-    pub_date = models.DateTimeField("Дата добавления", auto_now_add=True, db_index=True)
+    pub_date = models.DateTimeField(auto_now_add=True, db_index=True)
